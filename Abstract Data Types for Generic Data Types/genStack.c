f342cd5d966bd394951dcbe44f5fb236e6ec8c53
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-04-20 02:47:45 -0300

    Add files via upload

---
+++ Abstract Data Types for Generic Data Types/genStack.c
@@ -0,0 +1,92 @@
+#include "genStack.h"
+
+gStack* stInit(impressFunctionStack ifunc) {
+    gStack* newstack = (gStack *) malloc (sizeof(gStack));
+    newstack->counter = (size_t)0;
+    newstack->top = NULL;
+    newstack->ifunc = ifunc;
+
+    return newstack;
+}
+
+
+bool stIsEmpty(gStack* st) {
+    if (!st) return 1;
+    if (st->counter <= (size_t)0) return 1;
+    return 0;
+}
+
+
+void stDestroy(gStack **st) {
+    if (!st) return;
+    if (!(*st)) return;
+
+    if (stIsEmpty((*st))) {
+        free(*st);
+        *st = NULL;
+        return;
+    }
+
+    gNodeStack* aux = (*st)->top;
+    while (aux) {
+        (*st)->top = (*st)->top->next;
+        free(aux);
+        aux = (*st)->top;
+    }
+
+    free(*st);
+    *st = NULL;
+    return;
+}
+
+
+void stPush(gStack* st, void* data) {
+    if (!st) return;
+
+    gNodeStack* newnode = (gNodeStack *) malloc (sizeof(gNodeStack));
+    if (newnode == NULL) {
+        fprintf(stderr, "Failed to allocate memory for a new stack element.");
+        exit(EXIT_FAILURE);
+    }
+
+    (st->counter++);
+    newnode->data = data;
+    newnode->next = st->top;
+    st->top = newnode;
+    return;
+}
+
+
+void *stPop(gStack* st) {
+    if (!st) return NULL;
+    if (stIsEmpty(st)) return NULL;
+
+    (st->counter--);
+    gNodeStack* aux = st->top;
+    st->top = st->top->next;
+
+    return aux;
+}
+
+
+size_t stCount(gStack* st) {
+    if (!st) return (size_t)0;
+    return st->counter;
+}
+
+
+void impressStack(gStack* st) {
+    if (!st) { printf("[]\n"); return; }
+    if (stIsEmpty(st)) { printf("[]\n"); return; }
+
+    gNodeStack* aux = st->top;
+    printf("[");
+    while (aux) {
+        st->ifunc(aux->data);
+        printf(", ");
+        aux = aux->next;
+    }
+    printf("\b\b]");
+
+    return;
+}
---
+++ Abstract Data Types for Generic Data Types/genStack.h
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef GENSTACK_H
+#define GENSTACK_H
+
+typedef void (*impressFunctionStack)(void* data);
+
+typedef struct GENERICNODESTACK {
+    void* data;
+    struct GENERICNODESTACK* next;
+} gNodeStack;
+
+
+typedef struct GENERICSTACK {
+    gNodeStack* top;
+    size_t counter;
+    impressFunctionStack ifunc;
+} gStack;
+
+
+gStack* stInit(impressFunctionStack ifunc);          // Initializes the stack;
+void stDestroy(gStack **st);                         // Destroys the Stack;
+bool stIsEmpty(gStack *st);                          // Checks if the Stack is Empty;
+void stPush(gStack *st, void* data);                 // Inserts Elements into the Stack;
+void* stPop(gStack *st);                             // Returns Void Pointers to Elements Removed from the Stack.
+size_t stCount(gStack *st);                          // Returns the Number of Elements in the Stack;
+void impressStack(gStack *st);                       // Displays the Stack on the Terminal in List Format.
+
+#endif
+
+//
+// Example of Using the Stack in a Program:
+//
+/*
+
+void impressf(void *data) {
+    printf("%i", *((int *)data));
+    return;
+}
+
+int main(void) {
+
+    gStack *integerStack = stInit(impressf); // Initializing the stack.
+
+    int val1 = 2, val2 = 3, val3 = 1;        // Creating Elements of ANY TYPE to stack,
+                                             // the choice of objects of type int was
+                                             // completely arbitrary in this case.
+
+    stPush(integerStack, &val1);             // Stacking the First element.
+    stPush(integerStack, &val2);             // Stacking the Second element.
+    stPush(integerStack, &val3);             // Stacking the Third element.
+    impressStack(integerStack);              // Displaying the stack in the terminal.
+    stPop(integerStack);                     // Removing the element present at the top of the stack.
+    impressStack(integerStack);              // Displaying the stack in the terminal.
+
+    stDestroy(&integerStack);                // Don't forget to deallocate the memory present on the stack when closing the program.
+
+    return 0;
+}
+
+*/
