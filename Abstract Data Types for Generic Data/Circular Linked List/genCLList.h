c0faa22f59df9151f984a06a699acd07b93a41c9
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-04-23 01:56:40 -0300

    Rectifications and Creation of the Circular Linked List

--- Abstract Data Types for Generic Data/Circular Linked List/genCLList.h
+++ Abstract Data Types for Generic Data/Circular Linked List/genCLList.h
@@ -22,15 +22,15 @@ typedef struct {
 } gCLList;
 
 
-gCLList* initCLL();                              // Initialize the Circular Linked List.
-void* getCLCurrent(gCLList* cll);                // Returns the data of current element of the circular list.
-void clInsert(gCLList* cll, void* data);         // Inserts a new element after the current element.
-void* lcDeleteCurrent(gCLList *cll);             // Remove the current element, returning its value as a result.
-void* clDelete(gCLList* cll, void* data);        // Remove a specific element, returning its value as a result.
-size_t clCount(gCLList* cll);                    // Returns the total number of elements in the circular list.
-void clDestroy(gCLList** cll);                   // Frees memory allocated to the circular linked list.
-bool clIsEmpty(gCLList* cll);                    // Informs whether the circular list is empty.
-void clNext(gCLList* cll);                       // Changes the current to the next element in the list.
-void clPrevious(gCLList *cll);                   // Changes the current to the previous element in the list.
+gCLList* initCLL(impressFunctionCLList printCLL, compareFunctionCLList compareCLL);     // Initialize the Circular Linked List.
+void* getCLCurrent(gCLList* cll);                                                       // Returns the data of current element of the circular list.
+void clInsert(gCLList* cll, void* data);                                                // Inserts a new element after the current element.
+void* lcDeleteCurrent(gCLList *cll);                                                    // Remove the current element, returning its value as a result.
+void* clDelete(gCLList* cll, void* data);                                               // Remove a specific element, returning its value as a result.
+size_t clCount(gCLList* cll);                                                           // Returns the total number of elements in the circular list.
+void clDestroy(gCLList** cll);                                                          // Frees memory allocated to the circular linked list.
+bool clIsEmpty(gCLList* cll);                                                           // Informs whether the circular list is empty.
+void clNext(gCLList* cll);                                                              // Changes the current to the next element in the list.
+void clPrevious(gCLList *cll);                                                          // Changes the current to the previous element in the list.
 
 #endif
