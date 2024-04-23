fab2892bc410c68bd189d1fa776028f2b3809ccb
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-04-23 01:57:30 -0300

    Update genCLList.h

--- Abstract Data Types for Generic Data/Circular Linked List/genCLList.h
+++ Abstract Data Types for Generic Data/Circular Linked List/genCLList.h
@@ -25,7 +25,7 @@ typedef struct {
 gCLList* initCLL(impressFunctionCLList printCLL, compareFunctionCLList compareCLL);     // Initialize the Circular Linked List.
 void* getCLCurrent(gCLList* cll);                                                       // Returns the data of current element of the circular list.
 void clInsert(gCLList* cll, void* data);                                                // Inserts a new element after the current element.
-void* lcDeleteCurrent(gCLList *cll);                                                    // Remove the current element, returning its value as a result.
+void* lcDeleteCurrent(gCLList* cll);                                                    // Remove the current element, returning its value as a result.
 void* clDelete(gCLList* cll, void* data);                                               // Remove a specific element, returning its value as a result.
 size_t clCount(gCLList* cll);                                                           // Returns the total number of elements in the circular list.
 void clDestroy(gCLList** cll);                                                          // Frees memory allocated to the circular linked list.
