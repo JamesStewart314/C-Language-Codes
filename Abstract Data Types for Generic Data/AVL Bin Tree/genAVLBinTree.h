ced935180b3cf9916cfda45db36edb00c011c0be
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-05-01 23:16:23 -0300

    Reordering of comments to contribute to their readability

--- Abstract Data Types for Generic Data/AVL Bin Tree/genAVLBinTree.h
+++ Abstract Data Types for Generic Data/AVL Bin Tree/genAVLBinTree.h
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <time.h>
 
+#ifndef GENAVLBINTREE_H
+#define GENAVLBINTREE_H
+
 #include "../Queue/genQueue.h"
 #include "../Stack/genStack.h"
 
@@ -42,6 +45,8 @@ size_t avlBinTreeCount(avlBinTree* tree);                                       
 void avlBinTreeTextRepr(avlBinTree* tree);                                                          // Displays an accurate linear textual representation of the structure and arrangement of the elements contained in the AVL Binary Tree;
 long long int avlBinTreeGetNodeHeight(avlBinTree* tree, void* data);                                // Determines the height of a given node in the AVL Binary Tree. If the given element is not contained in the Tree, the function returns the value (-1).
 
+
+#endif
 
 
 /* 
