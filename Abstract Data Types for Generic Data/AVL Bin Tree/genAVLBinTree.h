cfb41630cda9a548ec2645182b7914e5dc8ec54b
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-05-10 23:51:59 -0300

    Formal Generalization of Data Structures and Rectification of the Element Discarding Method

--- Abstract Data Types for Generic Data/AVL Bin Tree/genAVLBinTree.h
+++ Abstract Data Types for Generic Data/AVL Bin Tree/genAVLBinTree.h
@@ -38,23 +38,25 @@ typedef struct {
 
 
 gAVLBinTree* initgAVLBinTree(impressFunctionGenAVLBinTree printF, compareFunctionGenAVLBinTree compareF, destroyFunctionGenAVLBinTree destroyF);   // Appropriately initializes the AVL Binary Tree and returns a gAVLBinTree pointer to the newly initialized instance;
-bool gAVLBinTreeIsEmpty(gAVLBinTree* tree);                                                                 // Checks whether an AVL Binary Tree is empty, returning 1 if it is and 0 otherwise;
-void gAVLBinTreeInsert(gAVLBinTree* tree, Pointer data);                                                      // Inserts a new element into the AVL Binary Tree if it is not already contained there;
-gAVLBinTree* gAVLBinTreeCopy(gAVLBinTree* tree);                                                            // Creates an identical copy of a given AVL Binary Tree and returns a gAVLBinTree pointer to the newly created copy;
-Pointer gAVLBinTreeGetBiggest(gAVLBinTree* tree);                                                             // Returns a void pointer to the largest element contained in the AVL Binary Tree;
-Pointer gAVLBinTreeGetSmallest(gAVLBinTree* tree);                                                            // Returns a void pointer to the smallest element contained in the AVL Binary Tree;
-bool gAVLBinTreeIsEquals(gAVLBinTree* tree1, gAVLBinTree* tree2);                                           // This function determines whether two AVL Binary Trees are identical. If they are equal, the function will return the value 1, otherwise it will return 0;
-void gAVLBinTreeImpress(gAVLBinTree* tree);                                                                 // Displays a linear textual representation of the content present in the binary tree through a symmetric traversal in its structure;
-void gAVLBinTreeimpressByLevel(gAVLBinTree* tree);                                                          // Displays a linear textual representation of the content present in the binary tree through a path through levels in its structure;
-                                                                                                    
-void gAVLBinTreeDestroy(gAVLBinTree** tree);                                                                //  Function whose purpose is to remove memory allocated in an AVL Binary Tree structure. In order to avoid memory leaks, remember to 
-                                                                                                            // invoke this function by applying it to the AVL Binary Tree(s) created when you are finished using the structure(s) in your code;
+gAVLBinTree* gAVLBinTreeCopy(gAVLBinTree* tree);                                                                                                   // Creates an identical copy of a given AVL Binary Tree and returns a gAVLBinTree pointer to the newly created copy;
 
-void gAVLBinTreeRemove(gAVLBinTree* tree, Pointer data);                                                      // Removes a given element from an AVL Binary Tree if it is contained therein;
-bool gAVLBinTreeSearch(gAVLBinTree* tree, Pointer data);                                                      // Determines whether an element is present in the AVL Binary Tree. If the element is present, the function returns the value 1; otherwise it will return 0;
-size_t gAVLBinTreeCount(gAVLBinTree* tree);                                                                 // This function calculates the number of elements present in the binary tree and returns the value resulting from this count;
-void gAVLBinTreeTextRepr(gAVLBinTree* tree);                                                                // Displays an accurate linear textual representation of the structure and arrangement of the elements contained in the AVL Binary Tree;
-long long int gAVLBinTreeGetNodeHeight(gAVLBinTree* tree, Pointer data);                                      // Determines the height of a given node in the AVL Binary Tree. If the given element is not contained in the Tree, the function returns the value (-1).
+void gAVLBinTreeImpress(gAVLBinTree* tree);                                                                                                        // Displays a linear textual representation of the content present in the binary tree through a symmetric traversal in its structure;
+void gAVLBinTreeimpressByLevel(gAVLBinTree* tree);                                                                                                 // Displays a linear textual representation of the content present in the binary tree through a path through levels in its structure;
+void gAVLBinTreeTextRepr(gAVLBinTree* tree);                                                                                                       // Displays an accurate linear textual representation of the structure and arrangement of the elements contained in the AVL Binary Tree;
+                                                                                                    
+void gAVLBinTreeDestroy(gAVLBinTree** tree);                                                                                                       //  Function whose purpose is to remove memory allocated in an AVL Binary Tree structure. In order to avoid memory leaks, remember to 
+                                                                                                                                                   // invoke this function by applying it to the AVL Binary Tree(s) created when you are finished using the structure(s) in your code;
+
+void gAVLBinTreeInsert(gAVLBinTree* tree, Pointer data);                                                                                           // Inserts a new element into the AVL Binary Tree if it is not already contained there;
+void gAVLBinTreeRemove(gAVLBinTree* tree, Pointer data);                                                                                           // Removes a given element from an AVL Binary Tree if it is contained therein;
+bool gAVLBinTreeIsEmpty(gAVLBinTree* tree);                                                                                                        // Checks whether an AVL Binary Tree is empty, returning 1 if it is and 0 otherwise;
+bool gAVLBinTreeSearch(gAVLBinTree* tree, Pointer data);                                                                                           // Determines whether an element is present in the AVL Binary Tree. If the element is present, the function returns the value 1; otherwise it will return 0;
+bool gAVLBinTreeIsEquals(gAVLBinTree* tree1, gAVLBinTree* tree2);                                                                                  // This function determines whether two AVL Binary Trees are identical. If they are equal, the function will return the value 1, otherwise it will return 0;
+long long int gAVLBinTreeGetNodeHeight(gAVLBinTree* tree, Pointer data);                                                                           // Determines the height of a given node in the AVL Binary Tree. If the given element is not contained in the Tree, the function returns the value (-1).
+size_t gAVLBinTreeCount(gAVLBinTree* tree);                                                                                                        // This function calculates the number of elements present in the binary tree and returns the value resulting from this count;
+Pointer gAVLBinTreeGetBiggest(gAVLBinTree* tree);                                                                                                  // Returns a void pointer to the largest element contained in the AVL Binary Tree;
+Pointer gAVLBinTreeGetSmallest(gAVLBinTree* tree);                                                                                                 // Returns a void pointer to the smallest element contained in the AVL Binary Tree;
+
 
 #endif
 
@@ -90,7 +92,7 @@ int main(int argc, char** argv) {
 
     clear_screen();
 
-    gAVLBinTree* avlTreeOfIntegers = initgAVLBinTree(impressf, comparef);
+    gAVLBinTree* avlTreeOfIntegers = initgAVLBinTree(impressf, comparef, NULL);
     int val1 = 1, val2 = 2, val3 = 3, val4 = 4, val5 = 5;
     int antiVal1 = (-1), antiVal2 = (-2), antiVal3 = (-3), antiVal4 = (-4), antiVal5 = (-5);
 
