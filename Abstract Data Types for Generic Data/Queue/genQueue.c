03c4027a8f6b9f9cdbe7d3ea450fbaa1349843ed
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-05-21 23:46:20 -0300

    Development of replication functionality for the queue module and rectification of memory leaks present in the demo code

--- Abstract Data Types for Generic Data/Queue/genQueue.c
+++ Abstract Data Types for Generic Data/Queue/genQueue.c
@@ -1,39 +1,94 @@
 #include "genQueue.h"
 
-gQueue* initgQueue(impressFunctionGenQueue printF, compareFunctionGenQueue compareF, destroyFunctionGenQueue destroyF) {
+gQueue* initgQueue(impressFunctionGenQueue printF, compareFunctionGenQueue compareF,
+                   destroyFunctionGenQueue destroyF, deepcopyFunctionGenQueue deepcopyF) {
 
     if (!compareF) {
         fprintf(stderr, "Error: Compare Function is NULL when creating a new generic Queue.\n");
         return NULL;
     }
 
-    gQueue* newqueue = (gQueue *) malloc (sizeof(gQueue));
+    gQueue* newqueue = (gQueue *) malloc(sizeof(gQueue));
     if (newqueue == NULL) {
         fprintf(stderr, "Error: Failed while trying to allocate memory for a new generic Queue.\n");
         exit(EXIT_FAILURE);
         return NULL;
     }
 
-    newqueue->counter = (size_t)0;
+    newqueue->counter = (uint32_t)0;
     newqueue->front = newqueue->rear = NULL;
     newqueue->printF = printF;
     newqueue->compareF = compareF;
     newqueue->destroyF = destroyF;
+    newqueue->deepcopyF = deepcopyF;
 
     return newqueue;
 }
 
 
-bool gQueueIsEmpty(gQueue* q) { 
-    if (!q) return 1;
-    return (q->counter <= (size_t)0);
+gQueue* gQueueCopy(gQueue* q) {
+    if (!q) return NULL;
+
+    gQueue* newqueue = initgQueue(q->printF, q->compareF, q->destroyF, q->deepcopyF);
+
+    if (gQueueIsEmpty(q)) return newqueue;
+
+    gQueueNode* auxNode = q->front;
+    while (auxNode) {
+        gQueueEnqueue(newqueue, (q->deepcopyF ? (q->deepcopyF(auxNode->data)) : auxNode->data));
+        auxNode = auxNode->next;
+    }
+
+    return newqueue;
 }
 
 
-void gQueueEnqueue(gQueue* q, gQueuePointerData data) {
+void gQueueClear(gQueue* q) {
+    if (!q) return;
+    // Queue is already empty:
+    if (gQueueIsEmpty(q)) return;
+
+    gQueueNode* auxNode = q->front;
+    do {
+        q->front = q->front->next;
+        if (q->destroyF) q->destroyF(auxNode->data);
+        free(auxNode); auxNode = q->front;
+    } while (auxNode);
+
+    q->counter = (uint32_t)0;
+    q->front = q->rear = NULL;
+
+    return;
+}
+
+
+void gQueueDestroy(gQueue** qPointer) {
+    if (!qPointer) return;
+    if (!(*qPointer)) return;
+
+    if (gQueueIsEmpty(*qPointer)) {
+        free(*qPointer); (*qPointer) = NULL;
+        return;
+    }
+
+    gQueueNode* auxNode = (*qPointer)->front;
+
+    do {
+        (*qPointer)->front = (*qPointer)->front->next;
+        if ((*qPointer)->destroyF) (*qPointer)->destroyF(auxNode->data);
+        free(auxNode); auxNode = (*qPointer)->front;
+    } while (auxNode);
+
+    free(*qPointer); (*qPointer) = NULL;
+
+    return;
+}
+
+
+void gQueueEnqueue(gQueue* q, gQueueDataPtr data) {
     if (!q) return;
 
-    gQueueNode* newnode = (gQueueNode *) malloc (sizeof(gQueueNode));
+    gQueueNode* newnode = (gQueueNode *) malloc(sizeof(gQueueNode));
     if (newnode == NULL) {
         fprintf(stderr, "Error: Unable to allocate memory to a new generic Queue node.\n");
         exit(EXIT_FAILURE);
@@ -45,11 +100,12 @@ void gQueueEnqueue(gQueue* q, gQueuePointerData data) {
 
     if (gQueueIsEmpty(q)) {
         q->front = q->rear = newnode;
-    } else {
-        q->rear->next = newnode;
-        q->rear = newnode;
+        (q->counter)++;
+        return;
     }
 
+    q->rear->next = newnode;
+    q->rear = newnode;
     (q->counter)++;
 
     return;
@@ -74,61 +130,21 @@ void gQueueImpress(gQueue* q) {
 }
 
 
-void gQueueDestroy(gQueue** qPointer) {
-    if (!qPointer) return;
-    if (!(*qPointer)) return;
-
-    if (gQueueIsEmpty(*qPointer)) {
-        free(*qPointer); (*qPointer) = NULL;
-        return;
-    };
-
-    gQueueNode* auxNode = (*qPointer)->front;
-
-    do {
-        (*qPointer)->front = (*qPointer)->front->next;
-        if ((*qPointer)->destroyF) (*qPointer)->destroyF(auxNode->data);
-        free(auxNode); auxNode = (*qPointer)->front;
-    } while (auxNode);
-
-    free(*qPointer); (*qPointer) = NULL;
-
-    return;
-}
-
-
-bool gQueueSearch(gQueue* q, gQueuePointerData data) {
-    if (!q) return 0;
-    if (gQueueIsEmpty(q)) return 0;
-
-    gQueueNode* auxNode = q->front;
-    while(auxNode) {
-        if (q->compareF(auxNode->data, data) == 0) return 1;
-        auxNode = auxNode->next;
-    }
-
-    return 0;
-}
-
-
-void gQueueRemove(gQueue* q, gQueuePointerData data) {
+void gQueueRemove(gQueue* q, gQueueDataPtr data) {
     if (!q) return;
     if (gQueueIsEmpty(q)) return;
 
     gQueueNode* auxNode = q->front, *previous = NULL;
-    while(auxNode) {
-        if (q->compareF(auxNode->data, data) != 0) {
-            // Not the Searched Element:
-            previous = auxNode;
-            auxNode = auxNode->next;
-            continue;
-        }
-        // Comparison result == 0 => element found:
-        break;
+    while (auxNode) {
+        if (q->compareF(auxNode->data, data) == 0) break;
+        // Not the Searched Element:
+        previous = auxNode;
+        auxNode = auxNode->next;
     }
 
     // Element not Found:
     if (!auxNode) return;
+
     // Otherwise, element Found:
     (q->counter)--;
 
@@ -160,7 +176,111 @@ void gQueueRemove(gQueue* q, gQueuePointerData data) {
 }
 
 
-gQueuePointerData gQueueDequeue(gQueue* q) {
+bool gQueueIsEmpty(gQueue* q) { 
+    if (!q) return 1;
+    return (q->counter <= (uint32_t)0);
+}
+
+
+bool gQueueIsEquals(gQueue* q1, gQueue* q2) {
+    if (!q1 || !q2) return (!q1 && !q2);
+
+    if (q1->compareF != q2->compareF) {
+        fprintf(stderr, "Error: It is not possible to compare two generic queues whose comparison functions differ from each other.\n");
+        return 0;
+    }
+
+    if (gQueueIsEmpty(q1) || gQueueIsEmpty(q2)) return (gQueueIsEmpty(q1) && gQueueIsEmpty(q2));
+
+    if (q1->counter != q2->counter) return 0;
+
+    gQueueNode *auxNode1 = q1->front, *auxNode2 = q2->front;
+    while (auxNode1 && auxNode2) {
+        if (q1->compareF(auxNode1->data, auxNode2->data) != 0) return 0;
+        auxNode1 = auxNode1->next;
+        auxNode2 = auxNode2->next;
+    }
+
+    return (!auxNode1 && !auxNode2);
+}
+
+
+bool gQueueHasSameElements(gQueue* q1, gQueue* q2) {
+    if (!q1 || !q2) return (!q1 && !q2);
+
+    if (q1->compareF != q2->compareF) {
+        fprintf(stderr, "Error: It is not possible to compare two generic queues whose comparison functions differ from each other.\n");
+        return 0;
+    }
+
+    if (gQueueIsEmpty(q1) || gQueueIsEmpty(q2)) return (gQueueIsEmpty(q1) && gQueueIsEmpty(q2));
+
+    gQueueNode* auxNode = q1->front;
+    while (auxNode) {
+        if (gQueueCount(q1, auxNode->data) != gQueueCount(q2, auxNode->data)) return 0;
+        auxNode = auxNode->next;
+    }
+
+    return 1;
+}
+
+
+bool gQueueShareSameElements(gQueue* q1, gQueue* q2) {
+    if (!q1 || !q2) return (!q1 && !q2);
+
+    if (q1->compareF != q2->compareF) {
+        fprintf(stderr, "Error: It is not possible to compare two generic queues whose comparison functions differ from each other.\n");
+        return 0;
+    }
+
+    if (gQueueIsEmpty(q1) || gQueueIsEmpty(q2)) return (gQueueIsEmpty(q1) && gQueueIsEmpty(q2));
+
+    gQueueNode* auxNode = q1->front;
+    while (auxNode) {
+        if (!gQueueSearch(q2, auxNode->data)) return 0;
+        auxNode = auxNode->next;
+    }
+
+    return 1;
+}
+
+
+bool gQueueSearch(gQueue* q, gQueueDataPtr data) {
+    if (!q) return 0;
+    if (gQueueIsEmpty(q)) return 0;
+
+    gQueueNode* auxNode = q->front;
+    while (auxNode) {
+        if (q->compareF(auxNode->data, data) == 0) return 1;
+        auxNode = auxNode->next;
+    }
+
+    return 0;
+}
+
+
+uint32_t gQueueCount(gQueue* q, gQueueDataPtr data) {
+    if (!q) return (uint32_t)0;
+    if (gQueueIsEmpty(q)) return (uint32_t)0;
+
+    uint32_t auxCounter = (uint32_t)0;
+    gQueueNode* auxNode = q->front;
+    while (auxNode) {
+        if (q->compareF(auxNode->data, data) == 0) auxCounter++;
+        auxNode = auxNode->next;
+    }
+
+    return auxCounter;
+}
+
+
+uint32_t gQueueSize(gQueue* q) {
+    if (!q) return 0;
+    return q->counter;
+}
+
+
+gQueueDataPtr gQueueDequeue(gQueue* q) {
     if (!q) return NULL;
     if (gQueueIsEmpty(q)) return NULL;
 
@@ -168,33 +288,38 @@ gQueuePointerData gQueueDequeue(gQueue* q) {
     gQueueNode* auxNode = q->front;
     q->front = q->front->next;
 
-    gQueuePointerData returnData = auxNode->data;
-    free(auxNode) ; auxNode = NULL;
+    gQueueDataPtr returnData = auxNode->data;
+    free(auxNode); auxNode = NULL;
 
     return returnData;
 }
 
 
-size_t gQueueSize(gQueue* q) {
-    if (!q) return 0;
-    return q->counter;
+gQueueDataPtr gQueueGetBiggest(gQueue* q) {
+    if (!q) return NULL;
+    if (gQueueIsEmpty(q)) return NULL;
+
+    gQueueNode* auxNode = q->front;
+    gQueueDataPtr returnData = auxNode->data;
+    while (auxNode) {
+        if (q->compareF(auxNode->data, returnData) > 0) returnData = auxNode->data;
+        auxNode = auxNode->next;
+    }
+
+    return returnData;
 }
 
 
-void gQueueClear(gQueue* q) {
-    if (!q) return;
-    // Queue is already empty:
-    if (gQueueIsEmpty(q)) return;
+gQueueDataPtr gQueueGetSmallest(gQueue* q) {
+    if (!q) return NULL;
+    if (gQueueIsEmpty(q)) return NULL;
 
     gQueueNode* auxNode = q->front;
+    gQueueDataPtr returnData = auxNode->data;
     while (auxNode) {
-        q->front = q->front->next;
-        if (q->destroyF) q->destroyF(auxNode->data);
-        free(auxNode); auxNode = q->front;
+        if (q->compareF(auxNode->data, returnData) < 0) returnData = auxNode->data;
+        auxNode = auxNode->next;
     }
 
-    q->counter = (size_t)0;
-    q->front = q->rear = NULL;
-
-    return;
+    return returnData;
 }
--- Abstract Data Types for Generic Data/Queue/genQueue.h
+++ Abstract Data Types for Generic Data/Queue/genQueue.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifndef GENERICQUEUE_H
 #define GENERICQUEUE_H
@@ -16,38 +17,49 @@
  structure by modifying the preprocessing
  directive parameter right below:
 */
-typedef void* gQueuePointerData;
+typedef void* gQueueDataPtr;
 
-typedef void (*impressFunctionGenQueue)(gQueuePointerData data);
-typedef int (*compareFunctionGenQueue)(gQueuePointerData data1, gQueuePointerData data2);
-typedef void (*destroyFunctionGenQueue)(gQueuePointerData data);
+typedef void (*impressFunctionGenQueue)(gQueueDataPtr data);
+typedef int (*compareFunctionGenQueue)(gQueueDataPtr data1, gQueueDataPtr data2);
+typedef void (*destroyFunctionGenQueue)(gQueueDataPtr data);
+typedef gQueueDataPtr (*deepcopyFunctionGenQueue)(gQueueDataPtr data);
 
 
 typedef struct GENERICQUEUENODE {
-    gQueuePointerData data;
+    gQueueDataPtr data;
     struct GENERICQUEUENODE* next;
 } gQueueNode;
 
 
 typedef struct {
     gQueueNode *front, *rear;
-    size_t counter;
+    uint32_t counter;
     impressFunctionGenQueue printF;
     compareFunctionGenQueue compareF;
     destroyFunctionGenQueue destroyF;
+    deepcopyFunctionGenQueue deepcopyF;
 } gQueue;
 
 
-gQueue* initgQueue(impressFunctionGenQueue printF, compareFunctionGenQueue compareF, destroyFunctionGenQueue destroyF); // Initializes the Queue;
-void gQueueDestroy(gQueue** qPointer);                                                                                  // Destroys the Queue;
-void gQueueEnqueue(gQueue* q, gQueuePointerData data);                                                                  // Inserts Elements into the Queue;
-void gQueueImpress(gQueue* q);                                                                                          // Displays the Queue on the Terminal in List Format;
-void gQueueRemove(gQueue* q, gQueuePointerData data);                                                                   // Removes and returns a given element from the Queue. If it is not present, the function returns NULL;
-void gQueueClear(gQueue* q);                                                                                            // Removes all elements contained in the queue;
-bool gQueueSearch(gQueue* q, gQueuePointerData data);                                                                   // Checks whether an element is present in the Queue, returning 1 if it is and 0 otherwise;
-bool gQueueIsEmpty(gQueue* q);                                                                                          // Checks if the Queue is Empty;
-size_t gQueueSize(gQueue* q);                                                                                           // Returns the Number of Elements in the Queue;
-gQueuePointerData gQueueDequeue(gQueue* q);                                                                             // Returns Void Pointers to Elements Removed from the Queue.
+gQueue* initgQueue(impressFunctionGenQueue printF, compareFunctionGenQueue compareF,
+                   destroyFunctionGenQueue destroyF, deepcopyFunctionGenQueue deepcopyF);    // Initializes the queue and returns a pointer to it;
+
+gQueue* gQueueCopy(gQueue* q);                                                               // Creates a deep copy of the given queue and returns a pointer to the new queue;
+void gQueueClear(gQueue* q);                                                                 // Removes all elements contained in the given queue;
+void gQueueDestroy(gQueue** qPointer);                                                       // Destroys the queue and all elements in it;
+void gQueueEnqueue(gQueue* q, gQueueDataPtr data);                                           // Inserts Elements into the Queue;
+void gQueueImpress(gQueue* q);                                                               // Displays the Queue on the Terminal in List Format;
+void gQueueRemove(gQueue* q, gQueueDataPtr data);                                            // Removes a given element from the Queue;
+bool gQueueIsEmpty(gQueue *q);                                                               // Checks if the queue is empty. Returns 1 if empty and 0 otherwise;
+bool gQueueIsEquals(gQueue* q1, gQueue* q2);                                                 // Determines whether two queues are identical. The judging criteria consists of a quantitative, qualitative and structural correspondence of the elements in both queues;
+bool gQueueHasSameElements(gQueue* q1, gQueue* q2);                                          // Determines whether two queues have the same elements, disregarding ordering criteria in this analysis;
+bool gQueueShareSameElements(gQueue* q1, gQueue* q2);                                        // Determines whether two queues share the same elements, disregarding ordering and multiplicity criteria of elements;
+bool gQueueSearch(gQueue* q, gQueueDataPtr data);                                            // Verifies the presence of an element in the Queue, returning 1 if found and 0 otherwise;
+uint32_t gQueueCount(gQueue* q, gQueueDataPtr data);                                         // Counts the occurrences of a given element in the queue and returns the total obtained from this count;
+uint32_t gQueueSize(gQueue* q);                                                              // Returns the number of elements in the queue;
+gQueueDataPtr gQueueDequeue(gQueue* q);                                                      // Returns a pointer to the newly dequeued element from the queue;
+gQueueDataPtr gQueueGetBiggest(gQueue* q);                                                   // Returns a pointer to the biggest element present on the queue;
+gQueueDataPtr gQueueGetSmallest(gQueue* q);                                                  // Returns a pointer to the smallest element present on the queue.
 
 #endif
 
@@ -55,81 +67,215 @@ gQueuePointerData gQueueDequeue(gQueue* q);                                     
 // Example of Using the Queue in a Program:
 /*
 
+#include <string.h>
 #include "genQueue.h"
 
 
-int comparef(void* data1, void* data2);
-void impressf(void* data);
+typedef struct {
+    char* name;
+    unsigned int id;
+} Person;
+
+
+void impressFPerson(void* data);
+int compareFPerson(void* data1, void* data2);
+void destroyFPerson(void* data);
+void* deepcopyFPerson(void* data);
+void impressFIntegers(void* data);
+int compareFIntegers(void* data1, void* data2);
+Person* createPerson(char* name, unsigned int id); // Function determined to speed up the process of creating persons.
+
 
 int main(int argc, char** argv) {
 
-    gQueue* queueOfIntegers = initgQueue(impressf, comparef, NULL);     // Creating the Queue.
+    puts("*Creating a Queue of Persons.");
+    gQueue* queueOfPersons = initgQueue(impressFPerson, compareFPerson, destroyFPerson, deepcopyFPerson);
+    printf("Queue Created: "); gQueueImpress(queueOfPersons); printf("\n\n");
 
-    int val1 = 1, val2 = 2, val3 = 3;                                   // Creating Elements of ANY TYPE to enqueue,
-                                                                        // the choice of objects of type int was
-                                                                        // completely arbitrary in this case.
+    Person *person1 = createPerson("Ayrton", 19), *person2 = createPerson("James", 20), *person3 = createPerson(NULL, 17);
+    Person *person4 = createPerson("Carlos", 21), *person5 = createPerson("Davi", 18);
 
-    puts("Inserting elements into the queue:");
-    gQueueEnqueue(queueOfIntegers, &val1);                              // Inserting the first element in the Queue.
-    gQueueImpress(queueOfIntegers); printf("\n");                       // Displays the queue in the terminal.
+    puts("Inserting Persons into the queue:");
+    gQueueEnqueue(queueOfPersons, person1);
+    gQueueEnqueue(queueOfPersons, person2);
+    gQueueEnqueue(queueOfPersons, person3);
+    gQueueEnqueue(queueOfPersons, person4);
+    gQueueEnqueue(queueOfPersons, person5);
 
-    gQueueEnqueue(queueOfIntegers, &val2);                              // Inserting the second element in the Queue.
-    gQueueImpress(queueOfIntegers); printf("\n");                       // Displays the queue in the terminal.
+    gQueueImpress(queueOfPersons); printf("\n");
 
-    gQueueEnqueue(queueOfIntegers, &val3);                              // Inserting the second element in the Queue.
-    gQueueImpress(queueOfIntegers); printf("\n");                       // Showing the final Result.
+    puts("\n*Removing Person3 from the queue:");
+    gQueueRemove(queueOfPersons, person3);
+    gQueueImpress(queueOfPersons); printf("\n");
 
-    puts("\n*Removing 2 from the queue");
-    gQueueRemove(queueOfIntegers, &val2);                               // Removing Val2 from the Queue.
-    gQueueImpress(queueOfIntegers); printf("\n");                       // Displays the queue in the terminal.
-
-    int* dedequeued_value = (int *)gQueueDequeue(queueOfIntegers);      // Getting an element from the queue.
-                                                                        // If the queue is empty, a pointer to
-                                                                        // NULL will be returned.
-    if (dedequeued_value) {
-        printf("\nDequeued value: %i\n", *dedequeued_value);
+    Person* dequeuedPerson = gQueueDequeue(queueOfPersons);
+    if (dequeuedPerson) {
+        printf("\nDequeued Person: "); impressFPerson(dequeuedPerson); printf("\n");
+        destroyFPerson(dequeuedPerson); dequeuedPerson = NULL;
     }
 
-    gQueueImpress(queueOfIntegers); printf("\n\n");                     // Displays the queue in the terminal.
-    gQueueRemove(queueOfIntegers, &val2);                               // TRYING to remove Val2 - which is no longer present - from the queue.
+    gQueueImpress(queueOfPersons); printf("\n\n");
+    gQueueRemove(queueOfPersons, person3);                               // TRYING to remove person3 - which is no longer present - from the queue.
 
     puts("Replacing removed elements:");
-    gQueueEnqueue(queueOfIntegers, &val2);
-    gQueueEnqueue(queueOfIntegers, &val1);
-    gQueueImpress(queueOfIntegers); printf("\n\n");
+    gQueueEnqueue(queueOfPersons, createPerson("Ayrton", 19));
+    gQueueEnqueue(queueOfPersons, createPerson(NULL, 17));
+    gQueueImpress(queueOfPersons); printf("\n\n");
+
+    puts("*Copying the Queue of Persons:");
+    gQueue* copyQueueOfPersons = gQueueCopy(queueOfPersons);
+
     puts("After clearing all queue elements:");
-    gQueueClear(queueOfIntegers);                                       // Removing all elements contained in the queue.
-    gQueueImpress(queueOfIntegers); printf("\n");
+    gQueueClear(queueOfPersons);                                       // Removing all elements contained in the queue.
+    gQueueImpress(queueOfPersons); printf("\n");
+
+    puts("\n*Destroying Original Queue of Persons.");
+    gQueueDestroy(&queueOfPersons);                                    // Destroying the Queue before exiting.
+    gQueueImpress(queueOfPersons);                                     // TRYING to Display the released queue in the terminal.
+    puts("*Original Queue of Persons Destroyed.\n");
+
+    
+    puts("Displaying the new copy of the Persons queue, demonstrating its independence from the original queue that was recently destroyed:");
+    gQueueImpress(copyQueueOfPersons); printf("\n");
+    
+    gQueueDestroy(&copyQueueOfPersons);
 
 
-    gQueueDestroy(&queueOfIntegers);                                    // Cleaning the Queue before exiting.
-    gQueueImpress(queueOfIntegers); printf("\n");                       // TRYING to Display the released queue in the terminal.
+    puts("\nNow, let's create a simple integer queue, a type of data structure that does not require dynamic memory allocation.");
 
-    puts("Program Finished!");
+    puts("*Creating the Queue of Integers:");
+    gQueue* queueOfIntegers = initgQueue(impressFIntegers, compareFIntegers, NULL, NULL);
+    gQueueImpress(queueOfIntegers); printf("\n\n");
+
+    int val1 = 1, val2 = 2, val3 = 3, val4 = 4, val5 = 5;
+
+    puts("*Inserting values from 1 to 5 in the integer queue:");
+    gQueueEnqueue(queueOfIntegers, &val1);
+    gQueueEnqueue(queueOfIntegers, &val2);
+    gQueueEnqueue(queueOfIntegers, &val3);
+    gQueueEnqueue(queueOfIntegers, &val4);
+    gQueueEnqueue(queueOfIntegers, &val5);
+    gQueueImpress(queueOfIntegers); printf("\n\n");
+
+
+    puts("All previously performed operations are equally applicable to the integer queue, so it would be redundant to recreate them for this specific example.");
+
+    puts("We will demonstrate below that the replication function of queues remains fully functional even in the absence of an explicit copy method provided during queue creation.\nThis is due to the fact that in this particular case there is no dynamic memory allocation.");
+
+    puts("\n*Copying the Queue of Integers:");
+    gQueue* copyQueueOfIntegers = gQueueCopy(queueOfIntegers);
+    gQueueImpress(copyQueueOfIntegers); printf("\n\n");
+
+    puts("*Destroying the Original Queue of Integers:");
+    gQueueDestroy(&queueOfIntegers);
+
+    puts("\nThe original queue remains independently unchanged:");
+    gQueueImpress(copyQueueOfIntegers); printf("\n");
+
+    puts("\n*Destroying also the copy of the original Queue of Integers:");
+    gQueueDestroy(&copyQueueOfIntegers);
+
+    puts("\n\nProgram Finished!");
 
     return 0;
 }
 
 
-int comparef(void* data1, void* data2) {
-    int aux1 = *((int *)data1);
-    int aux2 = *((int *)data2);
+void impressFPerson(void* data) {
+    if (!data) return;
 
-    if (!aux1 || !aux2) {
-        if (!aux1 && !aux2) return 1;
-        return 0;
+    Person* originalPerson = (Person *)data;
+
+    printf("Person(%s, %u)", (originalPerson->name != NULL) ? originalPerson->name : "NULL", originalPerson->id);
+    return;
+}
+
+
+int compareFPerson(void* data1, void* data2) {
+    if (!data1 || !data2) {
+        if (!data1 && !data2) return 0;
+        return (data1 == NULL) ? (-1) : 1;
     }
+
+    Person* aux1 = (Person *)data1;
+    Person* aux2 = (Person *)data2;
+
+    do {
+        if (!(aux1->name) || !(aux2->name)) {
+            if (!(aux1->name) && !(aux2->name)) break;
+            return (aux1->name == NULL) ? (-1) : 1;
+        } else {
+            int nameComparisonResult = strcmp(aux1->name, aux2->name);
+            if (nameComparisonResult != 0) return nameComparisonResult;
+            break;
+        }
+
+    } while(false);
+
+    if (aux1->id > aux2->id) return 1;
+    if (aux1->id < aux2->id) return -1;
+    return 0;
+}
+
+
+void destroyFPerson(void* data) {
+    if (!data) return;
+
+    // Deallocating the memory reserved for the name:
+    if(((Person *)data)->name) free(((Person *)data)->name);
+    free(data); data = NULL;
+
+    return;
+}
+
+
+Person* createPerson(char* name, unsigned int id) {
+    Person* newPerson = (Person *) malloc(sizeof(Person));
+    if (newPerson == NULL) {
+        fprintf(stderr, "Error: Failed while trying to allocate memory for a new Person.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newPerson->id = id;
+    if (name == NULL) { newPerson->name = NULL; return newPerson; }
+    // debbuging:
+    // printf("Name Size: %d\n", strlen(name) + 1);
+    newPerson->name = (char *) malloc(strlen(name) + 1);
+    strcpy(newPerson->name, name);
+
+    return newPerson;
+}
+
+
+void* deepcopyFPerson(void* data) {
+    if (!data) return NULL;
+
+    Person* originalPerson = (Person *)data;
+    Person* newPerson = createPerson(originalPerson->name, originalPerson->id);
+
+    return newPerson;
+}
+
+
+void impressFIntegers(void* data) {
+    if (!data) return;
+    printf("%d", *(int *)data);
+    return;
+}
+
+
+int compareFIntegers(void* data1, void* data2) {
+    if (!data1 || !data2) {
+        if (!data1 && !data2) return 0;
+        return (data1 == NULL) ? (-1) : 1;
+    }
+
+    int aux1 = *(int *)data1;
+    int aux2 = *(int *)data2;
 
     if (aux1 > aux2) return 1;
     if (aux1 < aux2) return -1;
     return 0;
-}
-
-
-void impressf(void* data) {
-    int* aux = (int *)data;
-    if (aux != NULL) { printf("%d", *aux); } else { printf("NULL"); }
-    return;
 }
 
 */
