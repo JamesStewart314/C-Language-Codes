b2ced4baa38b3b36720f0c54ae1f52f5efadb8d1
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-05-26 02:51:22 -0300

    Created Generic BubbleSort and MergeSort

--- Sorting Algorithms for Generic Data/genSorters.c
+++ Sorting Algorithms for Generic Data/genSorters.c
@@ -1,6 +1,81 @@
 #include "genSorters.h"
 
-void gBubbleSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortVectorPtr arrayPointer, uint64_t arraySize, uint64_t dataSize) {
+
+void gMergeArrays(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortArrayPtr arrayPointer, uint64_t start, uint64_t middle, uint64_t end, uint64_t dataSize) {
+
+    uint64_t arraySize = (end - start + (uint64_t)1);
+
+    void* auxiliarMergeArray = (void *) malloc(dataSize * arraySize);
+    uint64_t auxIndexCounter = 0;
+
+    uint64_t auxCounter1 = start, auxCounter2 = middle;
+
+    while ((auxCounter1 < middle) && (auxCounter2 <= end)) {
+        int comparisonResult = compareF(arrayPointer + auxCounter1 * dataSize, arrayPointer + auxCounter2 * dataSize);
+        
+        if (comparisonResult < 0) {
+            swapF(auxiliarMergeArray + auxIndexCounter * dataSize, arrayPointer + auxCounter1 * dataSize);
+            auxIndexCounter++;
+            auxCounter1++;
+            continue;
+        }
+
+        if (comparisonResult > 0) {
+            swapF(auxiliarMergeArray + auxIndexCounter * dataSize, arrayPointer + auxCounter2 * dataSize);
+            auxIndexCounter++;
+            auxCounter2++;
+            continue;
+        }
+
+        // comparisonResult == 0:
+        swapF(auxiliarMergeArray + (auxIndexCounter++) * dataSize, arrayPointer + auxCounter1 * dataSize);
+        swapF(auxiliarMergeArray + (auxIndexCounter++) * dataSize, arrayPointer + auxCounter2 * dataSize);
+        auxCounter1++;
+        auxCounter2++;
+    }
+
+    while (auxCounter1 < middle) {
+        swapF(auxiliarMergeArray + auxIndexCounter * dataSize, arrayPointer + auxCounter1 * dataSize);
+        auxIndexCounter++;
+        auxCounter1++;
+    }
+
+    while (auxCounter2 <= end) {
+        swapF(auxiliarMergeArray + auxIndexCounter * dataSize, arrayPointer + auxCounter2 * dataSize);
+        auxIndexCounter++;
+        auxCounter2++;
+    }
+
+    for (uint64_t i = 0; i < arraySize; i++) {
+        swapF(auxiliarMergeArray + i * dataSize, arrayPointer + (start + i) * dataSize);
+    }
+
+    free(auxiliarMergeArray);
+}
+
+
+void gRecursiveMergeSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortArrayPtr arrayPointer, uint64_t start, uint64_t end, uint64_t dataSize) {
+    
+    uint64_t intervalSize = end - start + (uint64_t)1;
+
+    if (intervalSize == 0) return;
+    if (intervalSize == (uint64_t)1) return;
+    if (intervalSize == (uint64_t)2) {
+        if (compareF(arrayPointer + start * dataSize, arrayPointer + end * dataSize) > 0) swapF(arrayPointer + start * dataSize, arrayPointer + end * dataSize);
+        return;
+    }
+
+    uint64_t middlePoint = start + ((intervalSize - (uint64_t)1) / (uint64_t)2);
+    
+    gRecursiveMergeSort(compareF, swapF, arrayPointer, start, middlePoint - (uint64_t)1, dataSize);
+    gRecursiveMergeSort(compareF, swapF, arrayPointer, middlePoint, end, dataSize);
+    gMergeArrays(compareF, swapF, arrayPointer, start, middlePoint, end, dataSize);
+
+    return;
+}
+
+
+void gBubbleSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortArrayPtr arrayPointer, uint64_t arraySize, uint64_t dataSize) {
     if (!compareF) {
         fprintf(stderr, "Error: Cannot generic Bubble Sort the Array if the compare function is NULL.\n");
         return;
@@ -29,4 +104,22 @@ void gBubbleSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, gen
 
         if (changeMade == 0) return;
     }
+}
+
+
+void gMergeSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortArrayPtr arrayPointer, uint64_t arraySize, uint64_t dataSize) {
+    if (!compareF) {
+        fprintf(stderr, "Error: Cannot generic Merge Sort the Array if the compare function is NULL.\n");
+        return;
+    }
+
+    if (arraySize == 0 || dataSize == 0) {
+        if (arraySize == 0) fprintf(stderr, "Error: ArraySize parameter for generic Merge Sort function cannot be zero.\n");
+        if (dataSize == 0) fprintf(stderr, "Error: DataSize parameter for generic Merge Sort function cannot be zero.\n");
+        return;
+    }
+
+    gRecursiveMergeSort(compareF, swapF, arrayPointer, 0, arraySize - (uint64_t)1, dataSize);
+
+    return;
 }
--- Sorting Algorithms for Generic Data/genSorters.h
+++ Sorting Algorithms for Generic Data/genSorters.h
@@ -6,11 +6,13 @@
 #ifndef GENERICBUBBLESORTER_H
 #define GENERICBUBBLESORTER_H
 
-typedef void* genSortVectorPtr;
+typedef void* genSortArrayPtr;
+typedef void* genSortDataPtr;
 
-typedef int (*compareFunctionGenSort)(genSortVectorPtr data1, genSortVectorPtr data2);
-typedef void (*swapFunctionGenSort)(genSortVectorPtr data1, genSortVectorPtr data2);
+typedef int (*compareFunctionGenSort)(genSortDataPtr data1, genSortDataPtr data2);
+typedef void (*swapFunctionGenSort)(genSortDataPtr data1, genSortDataPtr data2);
 
-void gBubbleSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortVectorPtr arrayPointer, uint64_t arraySize, uint64_t dataSize);
+void gBubbleSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortArrayPtr arrayPointer, uint64_t arraySize, uint64_t dataSize);
+void gMergeSort(compareFunctionGenSort compareF, swapFunctionGenSort swapF, genSortArrayPtr arrayPointer, uint64_t arraySize, uint64_t dataSize);
 
 #endif
